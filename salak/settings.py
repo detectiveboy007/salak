f71a84f78912247908599f976bb146dbb3139212
unknown <kataunthong@hotmail.com>
2021-11-25 22:03:36 +0700

    make it better

--- salak/settings.py
+++ salak/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+a)x=l$&00vdhl5jxe8jw#2el15^%)b651jr_1-&&key!1f(&y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://salaktkk.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
